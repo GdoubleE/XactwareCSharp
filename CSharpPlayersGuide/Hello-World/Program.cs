e7dd4c1e52f41e5b3b6ee7ec54663345ffce10df
Josh Gee <jjgee@xactware.com>
2017-04-06 11:33:23 -0600

    updated Hello-World and Added Variables project
    Added Creating Variables and commented on Hello-World

--- CSharpPlayersGuide/Hello-World/Program.cs
+++ CSharpPlayersGuide/Hello-World/Program.cs
@@ -1,15 +1,22 @@
-﻿using System;
+﻿
+//This sets up collections of libraries for our program to use
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//This sets the namespace of our project
 namespace Hello_World
-{
+{   //Program is is the entry point for our code and will contain our methods
     class Program
     {
+        //Main is the primary method of our program 
         static void Main(string[] args)
         {
+            //to make it easy we try our code with console actions - they are easy to check and quick. 
+            Console.WriteLine("I'm sorry Dave, I can not do that!");
+            Console.ReadKey();
         }
     }
 }
