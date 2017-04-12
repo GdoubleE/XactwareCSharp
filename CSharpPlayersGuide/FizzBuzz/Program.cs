819a906fbf41e2663145cd6cadc46004d26fffae
Josh Gee <jjgee@xactware.com>
2017-04-12 11:24:07 -0600

    Fizz Buzz
    Fizz Buzz project

---
+++ CSharpPlayersGuide/FizzBuzz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int number = 1; number <= 100; number++)
+            {
+
+                if ((number % 3) == 0 && (number % 5) == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if((number % 5)==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if ((number % 3) == 0)
+                {
+                    Console.WriteLine("Fizz");
+                } 
+                else
+                {
+                    Console.WriteLine(number);
+                }
+
+            }
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
