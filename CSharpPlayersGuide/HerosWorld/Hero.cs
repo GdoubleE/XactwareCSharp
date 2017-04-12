57ec3815e0a781804ae91b754c2b4c7507309f24
Josh Gee <jjgee@xactware.com>
2017-04-12 11:32:50 -0600

    Hero's world
    Hero's world app

---
+++ CSharpPlayersGuide/HerosWorld/Hero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerosWorld
+{
+    class Hero
+    {
+        public void HeroCard()
+        {
+            string name;
+            string race;
+
+            int strength;
+            int endurance;
+            int charisma;
+            int agility;
+            int luck;
+            int totalValueMinimum = 35;
+            int playerTotalStats = 0;
+        }
+
+
+    }
+}
